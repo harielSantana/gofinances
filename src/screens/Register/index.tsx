f20bff4c411262f8360d153f3cf8c4b8d42b9be4
Hariel Santana <hariol.santana138@gmail.com>
2022-01-27 14:50:12 -0300

    feat(register.spec.tsx): teste de interação na tela de registro

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -146,6 +146,7 @@ export function Register() {
             </TransactionsTypes>
 
             <CategorySelectButton
+              testID="button-category"
               title={category.name}
               onPress={() => handleOpenSelectCategoryModal()}
             />
@@ -153,7 +154,7 @@ export function Register() {
           <Button title="Enviar" onPress={handleSubmit(handleRegister)} />
         </Form>
 
-        <Modal visible={categoryModalOpen}>
+        <Modal testID="modal-category" visible={categoryModalOpen}>
           <CategorySelect
             category={category}
             setCategory={setCategory}
