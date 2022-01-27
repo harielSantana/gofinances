4a8d24594d505efecb4651055111b8579aa36bf4
Hariel Santana <hariol.santana138@gmail.com>
2022-01-27 09:47:18 -0300

    refactor(auth.spec.tsx): atualização da lib de auth do google

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,32 +1,40 @@
 import { renderHook } from "@testing-library/react-hooks";
 import { act } from "@testing-library/react-native";
-import { mocked } from "ts-jest/utils";
 import fetchMock from "jest-fetch-mock";
+import { mocked } from "ts-jest/utils";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { AuthProvider, useAuth } from "./auth";
+import { startAsync } from "expo-auth-session";
 
 fetchMock.enableMocks();
 
 const userInfo = {
-  id: "any_id",
-  email: "john.doe@email.com",
-  name: "John Doe",
+  id: "1",
+  email: "hariel@email.com",
+  name: "Hariel",
   photo: "any_photo.png",
 };
 
-jest.mock("expo-auth-session", () => {
-  return {
-    startAsync: () => ({
+jest.mock("expo-auth-session");
+
+describe("Auth Hook", () => {
+  beforeEach(async () => {
+    const userCollectionKey = "@gofinances:user";
+    await AsyncStorage.removeItem(userCollectionKey);
+  });
+
+  it("should be able to sign in with an existing Google account", async () => {
+    const googleMock = mocked(startAsync as any);
+
+    googleMock.mockReturnValueOnce({
       type: "success",
       params: {
         access_token: "any_token",
       },
-    }),
-  };
-});
+    });
 
-describe("Auth Hook", () => {
-  it("should be able to sign in with an existing Google account", async () => {
     fetchMock.mockResponseOnce(JSON.stringify(userInfo));
 
     const { result } = renderHook(() => useAuth(), {
@@ -39,7 +47,11 @@ describe("Auth Hook", () => {
   });
 
   it("user should not connect if cancel authentication with google", async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(userInfo));
+    const googleMock = mocked(startAsync as any);
+
+    googleMock.mockReturnValueOnce({
+      type: "cancel",
+    });
 
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
