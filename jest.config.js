30eb00c734b11f2ac2f9921fea9cea6e28527443
Hariel Santana <hariol.santana138@gmail.com>
2022-01-27 16:04:04 -0300

    chore(coverage): relatório de testes com Jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   preset: "jest-expo",
+
   testPathIgnorePatterns: ["/node_modules", "/android", "/ios"],
+
   setupFilesAfterEnv: [
     "@testing-library/jest-native/extend-expect",
     "jest-styled-components",
   ],
+
   setupFiles: ["./setupFile.js"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.tsx", "!src/**/*.spec.tsx"],
+  coverageReporters: ["lcov"],
 };
